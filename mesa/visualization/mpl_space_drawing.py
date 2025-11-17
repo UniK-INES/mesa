ba1800edd57604039b0dfdee2c121396f44e218e
Sascha Holzhauer <Sascha.Holzhauer@uni-kassel.de>
2025-11-17 09:27:26 +0100

    correction in draw_space

--- mesa/visualization/mpl_space_drawing.py
+++ mesa/visualization/mpl_space_drawing.py
@@ -196,7 +196,7 @@ def draw_space(
 
     """
     if ax is None:
-        fig, ax = plt.subplots()
+        _, ax = plt.subplots()
 
     # https://stackoverflow.com/questions/67524641/convert-multiple-isinstance-checks-to-structural-pattern-matching
     match space:
@@ -434,7 +434,7 @@ def draw_orthogonal_grid(
 
     """
     if ax is None:
-        fig, ax = plt.subplots()
+        _, ax = plt.subplots()
 
     # gather agent data
     s_default = (180 / max(space.width, space.height)) ** 2
@@ -479,7 +479,7 @@ def draw_hex_grid(
     "size", "marker", "zorder", alpha, linewidths, and edgecolors. Other field are ignored and will result in a user warning.
     """
     if ax is None:
-        fig, ax = plt.subplots()
+        _, ax = plt.subplots()
 
     # gather data
     s_default = (180 / max(space.width, space.height)) ** 2
@@ -564,7 +564,7 @@ def draw_network(
 
     """
     if ax is None:
-        fig, ax = plt.subplots()
+        _, ax = plt.subplots()
     if layout_kwargs is None:
         layout_kwargs = {"seed": 0}
 
@@ -633,7 +633,7 @@ def draw_continuous_space(
 
     """
     if ax is None:
-        fig, ax = plt.subplots()
+        _, ax = plt.subplots()
 
     # space related setup
     width = space.x_max - space.x_min
@@ -685,7 +685,7 @@ def draw_voronoi_grid(
 
     """
     if ax is None:
-        fig, ax = plt.subplots()
+        _, ax = plt.subplots()
 
     x_list = [i[0] for i in space.centroids_coordinates]
     y_list = [i[1] for i in space.centroids_coordinates]
